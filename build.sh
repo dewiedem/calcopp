0a9f586094a601dd5a75fb89803df84b6430cd16
Dennis Wiedemann <dennis.wiedemann@chem.tu-berlin.de>
2019-04-11 16:29:08 +0200

    Work on build scripts

---
+++ build.sh
@@ -0,0 +1,106 @@
+#!/bin/bash -x
+#
+# This file is used to build/freeze CalcOPP on Linux systems. It relies on
+# GFortran (https://gcc.gnu.org/wiki/GFortran), Pandoc (https://pandoc.org/),
+# Python (https://www.python.org/), and UPX (https://upx.github.io/) being
+# in the system PATH.
+
+
+# Compile the Fortran modules "pdf2opp_2d" and "pdf2opp_3d"
+if [ ! -d "dist" ]; then
+    mkdir dist
+fi
+
+gfortran pdf2opp_2d.f08 \
+    -o dist\pdf2opp_2d.exe \
+    -std=f2008 \
+    -fall-intrinsics \
+    -pedantic \
+    -Wall \
+    -Wextra \
+    -O3 \
+    -fno-backtrace \
+    -static \
+    -s \
+    -m64
+
+gfortran pdf2opp_3d.f08 \
+    -o dist\pdf2opp_3d.exe \
+    -std=f2008 \
+    -fall-intrinsics \
+    -pedantic \
+    -Wall \
+    -Wextra \
+    -O3 \
+    -fno-backtrace \
+    -static \
+    -s \
+    -m64 
+
+
+# Create the documentation HTML files
+cd pandoc
+
+pandoc ..\README.md \
+    --from gfm \
+    --to html5 \
+    --output ..\docs\README.html \
+    --standalone \
+    --metadata-file=metadata.yaml \
+    --resource-path=.;.. \
+    --self-contained
+
+pandoc ..\CHANGELOG.md \
+    --from gfm \
+    --to html5 \
+    --output ..\docs\CHANGELOG.html \
+    --standalone \
+    --metadata-file=metadata.yaml \
+    --self-contained
+
+cd ..
+
+
+# Freeze the Python modules "sd2opp" and "calcopp-gui"
+python -O -m PyInstaller sd2opp.py \
+   --noconfirm \
+   --clean \
+   --onedir \
+   --log-level=WARN \
+   --nowindowed \
+   --icon=data\CalcOPP.ico \
+   --version-file=sd2opp_info.txt
+
+python -O -m PyInstaller calcopp-gui.py \
+   --name=CalcOPP \
+   --noconfirm \
+   --clean \
+   --onedir \
+   --log-level=WARN \
+   --add-data="data\citation.bib;data" \
+   --add-data="data\citation.ris;data" \
+   --add-data="data\logo.png;data" \
+   --add-data="data\CalcOPP.ico;data" \
+   --add-data="docs\README.html;docs" \
+   --add-data="docs\CHANGELOG.html;docs" \
+   --add-data="LICENSE;docs" \
+   --add-data="docs\BSD-2.0.txt;docs" \
+   --add-data="docs\LGPL-3.0.txt;docs" \
+   --add-data="dist\sd2opp\sd2opp.exe;." \
+   --add-data="dist\sd2opp\sd2opp.exe.manifest;." \
+   --add-data="dist\pdf2opp_2d.exe;." \
+   --add-data="dist\pdf2opp_3d.exe;." \
+   --icon=data\CalcOPP.ico \
+   --version-file=calcopp_info.txt
+# --noconsole \
+# Opening an underlying console window as a workaround for a crash on
+# exit via the X button, which is caused by some bad interplay between
+# PyInstaller, NumPy, and PySimpleGUI.
+
+
+# Pack the data for distribution
+cd dist
+
+zip -r9 CalcOPP.zip CalcOPP
+
+cd ..
